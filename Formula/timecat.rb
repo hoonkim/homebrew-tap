686af95e41d7ff4da3908a395e484c29f475fdaa
Hoon Kim <kimh@ihateflyingbugs.com>
2026-06-25 23:13:11 +0900

    Add timecat formula

---
+++ Formula/timecat.rb
@@ -0,0 +1,17 @@
+class Timecat < Formula
+  desc "Large 7-segment digital clock for modern terminals"
+  homepage "https://github.com/hoonkim/timecat"
+  url "https://github.com/hoonkim/timecat/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9bf73e2a637c0de5cc63b430d347b953b57c69fad6a3a69a26db8dd0ae342584"
+  head "https://github.com/hoonkim/timecat.git", branch: "develop"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", *std_cargo_args
+  end
+
+  test do
+    assert_match "timecat 0.1.0", shell_output("#{bin}/timecat --version")
+  end
+end
--- README.md
+++ README.md
@@ -9,3 +9,11 @@ A terminal Markdown viewer for [kitty](https://github.com/hoonkim/mdcat).
 ```bash
 brew install hoonkim/tap/mdcat
 ```
+
+## timecat
+
+A large 7-segment digital clock for modern terminals.
+
+```bash
+brew install hoonkim/tap/timecat
+```
